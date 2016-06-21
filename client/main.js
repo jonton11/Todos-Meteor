6db78e66f3cb281bd8ef1dd7a6447285a8cbbfdf
Jonathan <jon.wong92@gmail.com>
2016-06-21 16:20:33 -0700

    Added edit/save, complete/incomplete, and total counts

--- client/main.js
+++ client/main.js
@@ -3,8 +3,28 @@
 Todos = new Meteor.Collection('todos');
 
 Template.todos.helpers({
-  'todo': function() {
+  'todo': function(){
     return Todos.find({}, {sort: {createdAt: -1}});
+  }
+});
+
+Template.todoItem.helpers({
+  'checked': function(){
+    var isCompleted = this.completed;
+    if (isCompleted) {
+      return "checked";
+    } else {
+      return "";
+    }
+  }
+});
+
+Template.todoCount.helpers({
+  'totalTodos': function() {
+    return Todos.find().count();
+  },
+  'completedTodos': function() {
+    return Todos.find({completed: true}).count();
   }
 });
 
@@ -25,6 +45,25 @@ Template.todos.events({
     var confirm = window.confirm("Delete this task?");
     if (confirm) {
       Todos.remove({_id: documentId});
+    }
+  },
+  'keyup [name=todoItem]': function(event){
+    if (event.which == 13 || event.which == 27) {
+      $(event.target).blur();
+      // Utilize enter and esc keys to remove focus effect
+    } else {
+      var documentId = this._id;
+      var todoItem = $(event.target).val();
+      Todos.update({_id: documentId}, {$set: { name: todoItem }});
+    }
+  },
+  'change [type=checkbox]': function() {
+    var documentId = this._id;
+    var isCompleted = this.completed;
+    if (isCompleted) {
+      Todos.update({_id: documentId}, {$set: {completed: false}});
+    } else {
+      Todos.update({_id: documentId}, {$set: {completed: true}});
     }
   }
 });
