9701f6883ab5a7992ce1effd09d3365ba605b84f
Jonathan <jon.wong92@gmail.com>
2016-06-22 19:18:47 -0700

    Added jQuery validations and refactored error messages through Template hooks

--- client/main.js
+++ client/main.js
@@ -108,18 +108,6 @@ Template.addList.events({
 Template.register.events({
   'submit form': function(event){
     event.preventDefault();
-    var email = $('[name=email]').val();
-    var password = $('[name=password]').val();
-    Accounts.createUser({
-      email: email,
-      password: password
-    }, function(error){
-      if (error) {
-        console.log(error.reason);
-      } else {
-        Router.go("home");
-      }
-    });
     Router.go('home');
   }
 });
@@ -135,17 +123,81 @@ Template.navigation.events({
 Template.login.events({
   'submit form': function(event) {
     event.preventDefault();
-    var email = $('[name=email]').val();
-    var password = $('[name=password]').val();
-    Meteor.loginWithPassword(email, password, function(error) {
-      if (error) {
-        console.log(error.reason);
-      } else {
-        var currentRoute = Router.current().route.getName();
-        if (currentRoute == "login") {
-          Router.go('home');
-        }
-      }
-    });
   }
-})
+});
+
+// Template Hooks
+$.validator.setDefaults({
+  rules: {
+    email: {
+      required: true,
+      email: true
+    },
+    password: {
+      required: true,
+      minlength: 6
+    },
+    messages: {
+      email: {
+        required: "You must enter an email address",
+        email: "You've entered an invalid email address"
+      },
+      password: {
+        required: "You must enter a password",
+        minlength: "Your password must be at least {0} characters long"
+      }
+    }
+  }
+});
+
+Template.login.onRendered(function() {
+  var validator = $('.login').validate({
+    submitHandler: function(event){
+      var email = $('[name=email]').val();
+      var password = $('[name=password]').val();
+      Meteor.loginWithPassword(email, password, function(error) {
+        if (error) {
+          if (error.reason == "User not found") {
+            validator.showErrors({
+              email: "That email doesn't belong to a registered user"
+            });
+          }
+          if (error.reason == "Incorrect password") {
+            validator.showErrors({
+              password: "You entered an incorrect password"
+            });
+          }
+        } else {
+          var currentRoute = Router.current().route.getName();
+          if (currentRoute == "login") {
+            Router.go('home');
+          }
+        }
+      });
+    }
+  });
+
+});
+
+Template.register.onRendered(function() {
+  var validator = $('.register').validate({
+    submitHandler: function(event) {
+      var email = $('[name=email]').val();
+      var password = $('[name=password]').val();
+      Accounts.createUser({
+        email: email,
+        password: password
+      }, function(error) {
+        if (error) {
+          if (error.reason == "Email already exists.") {
+            validator.showErrors({
+              email: "That email is taken"
+            });
+          }
+        } else {
+          Router.go("home");
+        }
+      });
+    }
+  });
+});
--- server/main.js
+++ server/main.js
@@ -7,5 +7,16 @@ Meteor.startup(() => {
 
 });
 
+// Collections
 Todos = new Meteor.Collection('todos');
 Lists = new Meteor.Collection('lists');
+
+// TODO: Twitter OAuth
+// Accounts.loginServiceConfiguration.remove({
+//   service: "twitter"
+// });
+// Accounts.loginServiceConfiguration.insert({
+//   service: "twitter",
+//   consumerKey: "VSbtq8ciL5W7BgVhbyxbpJU2W",
+//   secret: "SzxuWy8EXP84QHm9Fu8KsUTJpupvK7s90bCR0QAywaeOWXQ1OH"
+// });
